d044e4e4043a1d870d458475144c72ff5a735005
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2020-08-24 13:59:24 +0200

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,4 +1,7 @@
 $host.UI.RawUI.WindowTitle = "Security-App Uinstall"
+
+[string]$Root = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+
 Write-Host -ForegroundColor Yellow "[INFO] `t Removing clink."
 Start-Process cmd.exe -ArgumentList '/c choco uninstall clink -y' -Wait
 Write-Host -ForegroundColor Yellow "[OK] `t Done removing clink."
@@ -9,9 +12,11 @@ Remove-Item -Path C:\ProgramData\chocolatey -Recurse -Force
 Write-Host -ForegroundColor Yellow "[OK] `t Done uninstalling chocolatey."
 "`n"
 Write-Host -ForegroundColor Yellow "[INFO] `t Deleting direcories."
-$Files = Get-ChildItem -Path $PSScriptRoot -Recurse
+$Files = Get-ChildItem -Path $Root -Recurse
 ForEach-Object($file in $Files)
 {
     Remove-Item -Path $file.FullName -Force
 }
 Write-Host -ForegroundColor Yellow "[OK] `t Done deleting direcories."
+Write-Output -InputObject "Press any key to continue..."
+[void]$host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
